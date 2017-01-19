7163c13259f1a77f5d380698757a3eb46a40c9f8
Getro Naissance <getro89@gmail.com>
2017-01-18 22:00:11 -0800

    it will turn each question into a link

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    update(rental, params) {
-      this.sendAction('update', rental, params);
-    },
-    delete(question) {
-      if(confirm('Are you sure you want to delete this rental?')) {
-        this.sendAction('destroyQuestion', question);
-      }
-    }
-  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,16 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
   actions: {
     addNewQuestion(params) {
       var neoQuestion = this.store.createRecord('question', params);
       neoQuestion.save();
-      this.transitionTo('index');
-    },
-
-    destroyQuestion(question) {
-      question.destroyRecord();
       this.transitionTo('index');
     }
   }
